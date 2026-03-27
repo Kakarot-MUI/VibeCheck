7e150de96887078ded028caf2dce0ad355bdb9ba
Kakarot-MUI <mp8719090@gmail.com>
2026-03-27 18:24:06 +0530

    Fix PathError again using named wildcard /:path*

--- server/index.mjs
+++ server/index.mjs
@@ -183,7 +183,7 @@ app.post('/api/profile/update', async (req, res) => {
 const distPath = path.join(__dirname, '../dist');
 if (fs.existsSync(distPath)) {
   app.use(express.static(distPath));
-  app.get('/*', (req, res) => {
+  app.get('/:path*', (req, res) => {
     if (!req.path.startsWith('/api')) {
       res.sendFile(path.join(distPath, 'index.html'));
     }
